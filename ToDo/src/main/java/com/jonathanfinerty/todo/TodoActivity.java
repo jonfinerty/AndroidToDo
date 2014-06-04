95e28f559aeceba8be5e74d26a57e58aaa34f60e
Jonathan Finerty <jonathanfinerty@gmail.com>
2014-06-04 21:02:04 +0100

    Save and read list from file

--- ToDo/src/main/java/com/jonathanfinerty/todo/TodoActivity.java
+++ ToDo/src/main/java/com/jonathanfinerty/todo/TodoActivity.java
@@ -8,6 +8,16 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class TodoActivity extends Activity {
@@ -22,12 +32,10 @@ public class TodoActivity extends Activity {
         setContentView(R.layout.activity_todo);
 
         lvItems = (ListView) findViewById(R.id.lvItems);
-        items = new ArrayList<String>();
+        loadItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
 
         lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
-        items.add("Second Item");
         setupListViewListener();
     }
 
@@ -37,6 +45,7 @@ public class TodoActivity extends Activity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 items.remove(position);
                 itemsAdapter.notifyDataSetChanged();
+                saveItems();
                 return true;
             }
         });
@@ -46,5 +55,42 @@ public class TodoActivity extends Activity {
         EditText editText = (EditText) findViewById(R.id.etNewItem);
         itemsAdapter.add(editText.getText().toString());
         editText.setText("");
+        saveItems();
+    }
+
+    private void loadItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+
+        try {
+            InputStream inputStream = new FileInputStream(todoFile);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            items = new ArrayList<String>();
+            while ((line = reader.readLine()) != null) {
+                items.add(line);
+            }
+            reader.close();
+        } catch (IOException exception) {
+            items = new ArrayList<String>();
+            exception.printStackTrace();
+        }
+    }
+
+    private void saveItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+
+        try {
+            OutputStream inputStream = new FileOutputStream(todoFile);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(inputStream));
+            for (String item : items) {
+                writer.write(item);
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
