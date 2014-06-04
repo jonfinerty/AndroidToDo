3c82544d5ad9d7fb7d3240fc8f2e8859acf39631
Jonathan Finerty <jonathanfinerty@gmail.com>
2014-06-04 20:26:14 +0100

    Add two items to the list

--- ToDo/src/main/java/com/jonathanfinerty/todo/TodoActivity.java
+++ ToDo/src/main/java/com/jonathanfinerty/todo/TodoActivity.java
@@ -2,14 +2,28 @@ package com.jonathanfinerty.todo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TodoActivity extends Activity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
     }
 }
